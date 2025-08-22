0ca7da258d9f6057039684f2516ce8ecce2ed3e6
kir <kirpro95@yandex.ru>
2025-08-22 20:46:10 +0300

    2. ManyToMany [#331986]

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "auto_post")
@@ -14,7 +16,14 @@ public class Post {
     private int id;
     private String description;
     private LocalDateTime created;
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne()
     @JoinColumn(name = "auto_user_id")
     private User user;
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = {@JoinColumn(name = "post_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
+    )
+    private List<User> participates = new ArrayList<>();
 }
